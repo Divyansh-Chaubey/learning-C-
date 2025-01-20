7c6b805ce605229349959cdb59ea42e6fdaf32d5
Divyansh-Chaubey <chaubeydivyansh91@gmail.com>
2025-01-20 19:25:54 +0530

    update

---
+++ pointer_to_structure.cpp
@@ -0,0 +1,31 @@
+# include<iostream>
+using namespace std;
+struct stu{
+    int roll;
+    string name;
+    string course;
+    void ask (){
+        cout<<"enter your rollno";
+        cin>>roll;
+        cout<<"enter your name";
+        cin>>name;
+        cout<<"enter your course";
+        cin>>course;
+    }
+
+    void dis(){
+        cout<<roll<<endl;
+        cout<<name<<endl;
+        cout<<course<<endl;
+    }
+};
+
+int main()
+{
+    stu a, *ptr;
+    a.ask();
+    ptr=&a;
+    cout<<ptr->name;
+
+    return 0;
+}
