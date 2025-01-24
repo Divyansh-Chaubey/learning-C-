41052f2b1ed5fae70ecb0dd821fad9ac263e15c6
Divyansh-Chaubey <chaubeydivyansh91@gmail.com>
2025-01-24 18:00:07 +0530

    update

---
+++ polymorphysm.cpp
@@ -0,0 +1,16 @@
+// polymorphysm means many forms 
+//  function ,operator,object 
+// type fo polymorphysm is compile and run time 
+// 
+
+
+# include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+    
+    return 0;
+}
