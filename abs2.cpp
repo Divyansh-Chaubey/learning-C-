19ed5c98892213c9d5f95f6d0dac3066e0a7a00b
Divyansh-Chaubey <chaubeydivyansh91@gmail.com>
2025-01-27 18:16:19 +0530

    update

--- abs2.cpp
+++ abs2.cpp
@@ -1,19 +1,57 @@
+// # include<iostream>
+// using namespace std;
+// class ra{
+//     public:
+//     void show();
+// };
+// class rat :public ra{
+//     public:
+//     void show(){
+//         cout<<"Rat";
+//     }
+// };
+
+// int main()
+// {
+//     rat obj;
+//     obj.show();
+//     return 0;
+// }
+
+// abstract class with the help of pure virtual function
+// void show() =0   pure function 
+// virtual void show() =0  pure virtual function
+// common genaralize interface
+// one *obj=new student()  
+
 # include<iostream>
 using namespace std;
-class ra{
+
+class person{
     public:
-    void show();
+    virtual void show ()=0;
 };
-class rat :public ra{
+class student :public person{
     public:
-    void show(){
-        cout<<"Rat";
+    virtual void show (){
+        string name= "radha";
+        int id=12;
+        cout<< name <<"\t"<<id;
+    }
+};
+class teacher:public person{
+    public:
+    virtual void show (){
+        string name= "rani";
+        int id=123;
+        cout<< name <<"\t"<<id;
     }
 };
 
+
 int main()
 {
-    rat obj;
-    obj.show();
+    person *obj=new teacher;
+    obj->show();
     return 0;
 }
